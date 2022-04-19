0abf138098eeccf5d678584c3623fbad59ad073b
PetxCode <brighterdayscodelab@gmail.com>
2022-04-19 16:20:21 +0100

    COMSOL API

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const port = 3550;
+require("./utils/database");
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("The best platform for all NGOs in Africa...!");
+});
+
+app.use("/api/user", require("./router/userRouter"));
+
+app.use("/api/user", require("./other/register"));
+app.use("/api/brand", require("./builds/brandRouter"));
+app.use("/api/event", require("./builds/eventRouter"));
+
+app.use("/api/pitch", require("./builds/pitchRouter"));
+app.use("/api/video", require("./builds/videoRouter"));
+app.use("/api/journey", require("./builds/journeyRouter"));
+
+app.listen(port, () => {
+	console.log("server is ready for connection");
+});
---
+++ model/pitchModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const pitchModel = mongoose.Schema(
+	{
+		title: {
+			type: String,
+		},
+		detail: {
+			type: String,
+		},
+		pitchDeck: {
+			type: String,
+		},
+		pitchDeckID: {
+			type: String,
+		},
+
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "users",
+		},
+	},
+	{ timestamps: true }
+);
+
+module.exports = mongoose.model("pitchs", pitchModel);
